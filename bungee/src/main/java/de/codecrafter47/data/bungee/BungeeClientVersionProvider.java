c6760b388fed128bc4e0726e9aac2d2d01719a5f
Florian Stober <flo.e3@gmx.de>
2020-06-25 10:02:45 +0200

    add 1.16.1 to client version list

--- bungee/src/main/java/de/codecrafter47/data/bungee/BungeeClientVersionProvider.java
+++ bungee/src/main/java/de/codecrafter47/data/bungee/BungeeClientVersionProvider.java
@@ -26,6 +26,8 @@ public class BungeeClientVersionProvider implements Function<ProxiedPlayer, Stri
     public String apply(ProxiedPlayer player) {
         switch (player.getPendingConnection().getVersion()) {
             // todo find some way to automatically update this
+            case 736:
+                return "1.16.1";
             case 735:
                 return "1.16";
             case 578:
