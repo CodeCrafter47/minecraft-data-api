ae6d3f097ea33d10a9fd200e8f13f1fff5f27f59
Florian Stober <flo.e3@gmx.de>
2021-07-05 10:51:30 +0200

    allow adding/ removing data change listeners while notifying listeners

--- api/src/main/java/de/codecrafter47/data/api/DataCache.java
+++ api/src/main/java/de/codecrafter47/data/api/DataCache.java
@@ -19,13 +19,14 @@ package de.codecrafter47.data.api;
 
 import com.google.common.collect.Sets;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DataCache implements DataHolder {
     private final Map<DataKey<?>, Object> cache = new ConcurrentHashMap<>();
-    private final Map<DataKey<?>, Set<Runnable>> listenerMap = new ConcurrentHashMap<>();
+    private final Map<DataKey<?>, ListenerMapEntry> listenerMap = new ConcurrentHashMap<>();
 
     @SuppressWarnings("unchecked")
     public <T> void updateValue(DataKey<T> dataKey, T object) {
@@ -34,12 +35,15 @@ public class DataCache implements DataHolder {
         } else {
             cache.put(dataKey, object);
         }
-        Set<Runnable> listeners = listenerMap.get(dataKey);
+        ListenerMapEntry entry = listenerMap.get(dataKey);
         AssertionError error = null;
-        if (listeners != null) {
-            for (Runnable listener : listeners) {
+        if (entry != null) {
+            entry.isNotifyingListeners = true;
+            for (Runnable listener : entry.listeners) {
                 try {
-                    listener.run();
+                    if (!entry.listenersRemoved.contains(listener)) {
+                        listener.run();
+                    }
                 } catch (Throwable th) {
                     if (error == null) {
                         error = new AssertionError("Failed to run listener", th);
@@ -47,6 +51,14 @@ public class DataCache implements DataHolder {
                         error.addSuppressed(th);
                     }
                 }
+            }
+            entry.listeners.removeAll(entry.listenersRemoved);
+            entry.listeners.addAll(entry.listenersAdded);
+            entry.listenersRemoved.clear();
+            entry.listenersAdded.clear();
+            entry.isNotifyingListeners = false;
+            if (entry.listeners.isEmpty()) {
+                listenerMap.remove(dataKey);
             }
         }
         if (error != null) {
@@ -62,21 +74,46 @@ public class DataCache implements DataHolder {
 
     @Override
     public <T> void addDataChangeListener(DataKey<T> key, Runnable listener) {
-        listenerMap.computeIfAbsent(key, k -> Sets.newConcurrentHashSet()).add(listener);
+        ListenerMapEntry entry = listenerMap.computeIfAbsent(key, k -> new ListenerMapEntry());
+        if (entry.isNotifyingListeners) {
+            entry.listenersRemoved.remove(listener);
+            entry.listenersAdded.add(listener);
+        } else {
+            entry.listeners.add(listener);
+        }
     }
 
     @Override
     public <T> void removeDataChangeListener(DataKey<T> key, Runnable listener) {
-        Set<Runnable> listeners = listenerMap.get(key);
-        if (listeners != null) {
-            listeners.remove(listener);
-            if (listeners.isEmpty()) {
-                listenerMap.remove(key);
+        ListenerMapEntry entry = listenerMap.get(key);
+        if (entry != null) {
+            if (entry.isNotifyingListeners) {
+                entry.listenersAdded.remove(listener);
+                entry.listenersRemoved.add(listener);
+            } else {
+                entry.listeners.remove(listener);
+                if (entry.listeners.isEmpty()) {
+                    listenerMap.remove(key);
+                }
             }
         }
     }
 
     public boolean hasListeners(DataKey<?> key) {
         return listenerMap.containsKey(key);
+    }
+
+    private static class ListenerMapEntry {
+        private final Set<Runnable> listeners;
+        private boolean isNotifyingListeners;
+        private final Set<Runnable> listenersAdded;
+        private final Set<Runnable> listenersRemoved;
+
+        public ListenerMapEntry() {
+            this.listeners = Sets.newConcurrentHashSet();
+            this.isNotifyingListeners = false;
+            this.listenersAdded = new HashSet<>();
+            this.listenersRemoved = new HashSet<>();
+        }
     }
 }
